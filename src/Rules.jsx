333677324627cee6e0f7cf34236d54165a5b2078
johnjohnjohn23 <slapmeonce101@gmail.com>
2024-01-15 23:20:18 +0800

    Change icons and image

---
+++ src/Rules.jsx
@@ -0,0 +1,334 @@
+import footerLogo from "./assets/FooterLogo.png";
+import amaroLogo from "./assets/AmaroResort.png";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faUserEdit,
+  faEnvelope,
+  faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+
+import "./Rules.css";
+
+function Rules() {
+  return (
+    <>
+      <section id="navBar" class="page-section active">
+        {/* start of nav */}
+        <div class="nav" id="nav">
+          <div class="site-nav">
+            <div class="site-name" id="site-name">
+              <img src={amaroLogo} alt="logo" className="logo" />
+            </div>
+            <div class="nav-bar" id="navbar">
+              <a class="navBar" href="index.php">
+                {" "}
+                Home{" "}
+              </a>
+              <a class="navBar" href="about.html">
+                {" "}
+                About{" "}
+              </a>
+              <a class="navBar" href="services.html">
+                {" "}
+                Services{" "}
+              </a>
+              <a class="navBar" href="reservation.html">
+                {" "}
+                Reservation{" "}
+              </a>
+              <a class="navBar" href="contact.html">
+                {" "}
+                Contact{" "}
+              </a>
+            </div>
+
+            <a href="#" id="book-button">
+              <button type="button" class="book-btn">
+                Book Now
+              </button>
+            </a>
+
+            <div class="vl"></div>
+
+            <span>
+              <FontAwesomeIcon icon={faUserEdit} id="user" />
+            </span>
+          </div>
+        </div>
+        {/* end of nav */}
+      </section>
+
+      {/* start of content */}
+      <div class="title">
+        <h1>Rules and Regulations</h1>
+      </div>
+
+      <div class="content">
+        <section id="rules">
+          <div>
+            <h4>Dear Valued Guests,</h4>
+            <h5>
+              {" "}
+              To make your stay safe and comfortable, please observe the
+              following rules and regulations.
+            </h5>
+          </div>
+
+          <div class="text1">
+            <p>
+              1. <span class="txt1">NO WRISTBAND NO ENTRY</span>, Guest must be
+              wearing their wristband at all times.
+            </p>
+          </div>
+
+          <div class="text2">
+            <p>
+              2. <span class="txt2">NO</span> downpayment{" "}
+              <span class="txt2">NO</span> reservation. Strictly{" "}
+              <span class="txt2">NO</span> cancellations{" "}
+              <span class="txt2">NO</span> refund policy. (Payment{" "}
+              <span class="indent2">
+                made for an accommodations in NON-transferable.)
+              </span>
+            </p>
+          </div>
+
+          <div class="text3">
+            <p>
+              3. Maong shorts/pants and colored and cotton t-shirts are{" "}
+              <span class="txt3">NOT ALLOWED</span> as swimming attires.
+            </p>
+          </div>
+
+          <div class="text4">
+            <p>
+              4. Strictly <span class="txt4">NO HARD DRINKS</span> (ex.
+              Empilights, redhorse, Alfonso and/or any alcoholic drinks with{" "}
+              <span class="indent4">
+                above 5% level of alcohol content). We only allow light beers
+                below 5% level of alcoholic content like pilsen, sanmiglight,
+                tanduay ice, with 10-20 pesos corkage fee per bottle if you have
+                your own light beers.
+              </span>
+            </p>
+            <span class="indent4"></span>
+          </div>
+
+          <div class="text5">
+            <p>
+              5. Gas stove{" "}
+              <span class="txt5">
+                butane, superkalan electrical electrical appliances are
+                prohibited
+              </span>
+              .
+            </p>
+          </div>
+
+          <div class="text6">
+            <p>
+              6. Speakers size above 6 inc is{" "}
+              <span class="txt6">prohibited,</span> loud system is not allowed
+              please respect the privacy{" "}
+              <span class="indent6">of other guests.</span>
+            </p>
+          </div>
+
+          <div class="text7">
+            <p>
+              7. Resort security has the right to inspect every persoons bags,
+              parcels or other items upon their{" "}
+              <span class="indent7">admission.</span>
+            </p>
+          </div>
+
+          <div class="text8">
+            <p>
+              8. WE are a PET FRIENDLY resort and follows strict PET POLICY. (We
+              allowed pets to enter only if{" "}
+              <span class="indent8">
+                they were in their cage and wearing their pampers).
+              </span>
+            </p>
+          </div>
+
+          <div class="text9">
+            <p>
+              9. Avoid littering. This is Clean and Green Resort. Common
+              trashcans are available at the designed{" "}
+              <span class="indent9">places within the resort.</span>
+            </p>
+          </div>
+
+          <div class="text10">
+            <p>
+              10. The resort cannot be responsible for accident or injury of any
+              of our guests, nor for the lost of{" "}
+              <span class="indent10">
+                valuables, money or property of any kind.
+              </span>
+            </p>
+          </div>
+
+          <div class="text11">
+            <p>
+              11. Prohibited drugs and firearms are not allowed. Please
+              surrender Firearms to the front desk for{" "}
+              <span class="indent11">safekeeping.</span>
+            </p>
+          </div>
+
+          <div class="text12">
+            <p>
+              12. Strictly <span class="txt12">NO TIME EXTENTION,</span> we
+              prioritize our cleaning breaks.
+            </p>
+          </div>
+
+          <div class="text13">
+            <p>13. Prices are subject to change without prior notice.</p>
+          </div>
+
+          <div class="text14">
+            <p>
+              14. We reserve the right to deny admission or evic guest or group
+              of guest who violate our <span class="indent14">policies.</span>
+            </p>
+          </div>
+
+          <div class="text15">
+            <p>
+              We are putting in a lot of effort to provide you the cleanest
+              Resort and safety stay.
+            </p>
+          </div>
+
+          <div class="text16">
+            <p>Thank you!</p>
+          </div>
+        </section>
+      </div>
+      {/* end of content */}
+
+      {/* start of CTA */}
+      <section id="cta">
+        <div class="cta-bg">
+          <div class="cta-content">
+            <div class="cta-above">
+              <h1>Experience the perfect getaway!</h1>
+              <p>
+                Experience the perfect getaway at our resort, offering a range
+                of event packages, enticing room accommodations, and competitive
+                swimming rates for an unforgettable stay.
+              </p>
+            </div>
+            <div class="cta-below">
+              <a href="reservation.html" id="cta-button">
+                <button type="button" class="cta-btn">
+                  BOOK NOW
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* end of CTA */}
+
+      {/* start of footer */}
+      <section id="footer">
+        <div class="ftr-container">
+          <div class="ftr-content">
+            <div class="ftr-left">
+              <img src={footerLogo} alt="logo" className="ftr-logo" />
+              <p class="cta-text">
+                Book your unforgettable getaway now and experience the ultimate
+                relaxation at our resort!
+              </p>
+              <div class="fb-link">
+                <a href="#" id="fblink">
+                  <span>
+                    <FontAwesomeIcon icon={faFacebook} id="ftricon" />
+                  </span>
+                  <span class="label">Amaro Resort</span>
+                </a>
+              </div>
+              <div class="email-address">
+                <a href="#" id="emailadd">
+                  <span>
+                    <FontAwesomeIcon icon={faEnvelope} id="ftricon" />
+                  </span>
+                  <span class="label">amaroresort@gmail.com</span>
+                </a>
+              </div>
+            </div>
+
+            <div class="ftr-right">
+              <div class="links-label">
+                <h2>Links</h2>
+                <ul>
+                  <li>
+                    <a href="index.php">Home</a>
+                  </li>
+                  <li>
+                    <a href="about.html">About</a>
+                  </li>
+                  <li>
+                    <a href="services.html">Services</a>
+                  </li>
+                  <li>
+                    <a href="reservation.html">Reservation</a>
+                  </li>
+                  <li>
+                    <a href="contact.html">Contact</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="links-label">
+                <h2>Legal</h2>
+                <ul>
+                  <li>
+                    <a href="privacypolicy.html">Privacy Policy</a>
+                  </li>
+                  <li>
+                    <a href="t&c.html">Terms & Conditions</a>
+                  </li>
+                  <li>
+                    <a href="rules.html">Rules and Regulations</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="newsletter">
+                <h2>Newsletter</h2>
+                <p class="newsletter-text">
+                  Elevate your resort experience and be the first to receive
+                  exclusive promotions, exciting updates, and insider insights
+                  by subscribing to our newsletter.
+                </p>
+                <div class="subscribe">
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                  ></input>
+                  <button type="submit" title="subscribe">
+                    <FontAwesomeIcon icon={faPaperPlane} id="subButton" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="copyright">Copyright © 2023. All rights reserved.</div>
+        </div>
+      </section>
+      {/* end of footer */}
+    </>
+  );
+}
+export default Rules;
