02618cf89cb6e409ab14d8e3180389153ce28c02
Christian Roed Boyles <chrstnrdbyls@gmail.com>
2024-01-15 23:27:22 +0800

    Rename class to className

--- src/Rules.jsx
+++ src/Rules.jsx
@@ -18,43 +18,43 @@ import "./Rules.css";
 function Rules() {
   return (
     <>
-      <section id="navBar" class="page-section active">
+      <section id="navBar" className="page-section active">
         {/* start of nav */}
-        <div class="nav" id="nav">
-          <div class="site-nav">
-            <div class="site-name" id="site-name">
-              <img src={amaroLogo} alt="logo" className="logo" />
+        <div className="nav" id="nav">
+          <div className="site-nav">
+            <div className="site-name" id="site-name">
+              <img src={amaroLogo} alt="logo" classNameName="logo" />
             </div>
-            <div class="nav-bar" id="navbar">
-              <a class="navBar" href="index.php">
+            <div className="nav-bar" id="navbar">
+              <a className="navBar" href="index.php">
                 {" "}
                 Home{" "}
               </a>
-              <a class="navBar" href="about.html">
+              <a className="navBar" href="about.html">
                 {" "}
                 About{" "}
               </a>
-              <a class="navBar" href="services.html">
+              <a className="navBar" href="services.html">
                 {" "}
                 Services{" "}
               </a>
-              <a class="navBar" href="reservation.html">
+              <a className="navBar" href="reservation.html">
                 {" "}
                 Reservation{" "}
               </a>
-              <a class="navBar" href="contact.html">
+              <a className="navBar" href="contact.html">
                 {" "}
                 Contact{" "}
               </a>
             </div>
 
             <a href="#" id="book-button">
-              <button type="button" class="book-btn">
+              <button type="button" className="book-btn">
                 Book Now
               </button>
             </a>
 
-            <div class="vl"></div>
+            <div className="vl"></div>
 
             <span>
               <FontAwesomeIcon icon={faUserEdit} id="user" />
@@ -65,11 +65,11 @@ function Rules() {
       </section>
 
       {/* start of content */}
-      <div class="title">
+      <div className="title">
         <h1>Rules and Regulations</h1>
       </div>
 
-      <div class="content">
+      <div className="content">
         <section id="rules">
           <div>
             <h4>Dear Valued Guests,</h4>
@@ -80,50 +80,50 @@ function Rules() {
             </h5>
           </div>
 
-          <div class="text1">
+          <div className="text1">
             <p>
-              1. <span class="txt1">NO WRISTBAND NO ENTRY</span>, Guest must be
+              1. <span className="txt1">NO WRISTBAND NO ENTRY</span>, Guest must be
               wearing their wristband at all times.
             </p>
           </div>
 
-          <div class="text2">
+          <div className="text2">
             <p>
-              2. <span class="txt2">NO</span> downpayment{" "}
-              <span class="txt2">NO</span> reservation. Strictly{" "}
-              <span class="txt2">NO</span> cancellations{" "}
-              <span class="txt2">NO</span> refund policy. (Payment{" "}
-              <span class="indent2">
+              2. <span className="txt2">NO</span> downpayment{" "}
+              <span className="txt2">NO</span> reservation. Strictly{" "}
+              <span className="txt2">NO</span> cancellations{" "}
+              <span className="txt2">NO</span> refund policy. (Payment{" "}
+              <span className="indent2">
                 made for an accommodations in NON-transferable.)
               </span>
             </p>
           </div>
 
-          <div class="text3">
+          <div className="text3">
             <p>
               3. Maong shorts/pants and colored and cotton t-shirts are{" "}
-              <span class="txt3">NOT ALLOWED</span> as swimming attires.
+              <span className="txt3">NOT ALLOWED</span> as swimming attires.
             </p>
           </div>
 
-          <div class="text4">
+          <div className="text4">
             <p>
-              4. Strictly <span class="txt4">NO HARD DRINKS</span> (ex.
+              4. Strictly <span className="txt4">NO HARD DRINKS</span> (ex.
               Empilights, redhorse, Alfonso and/or any alcoholic drinks with{" "}
-              <span class="indent4">
+              <span className="indent4">
                 above 5% level of alcohol content). We only allow light beers
                 below 5% level of alcoholic content like pilsen, sanmiglight,
                 tanduay ice, with 10-20 pesos corkage fee per bottle if you have
                 your own light beers.
               </span>
             </p>
-            <span class="indent4"></span>
+            <span className="indent4"></span>
           </div>
 
-          <div class="text5">
+          <div className="text5">
             <p>
               5. Gas stove{" "}
-              <span class="txt5">
+              <span className="txt5">
                 butane, superkalan electrical electrical appliances are
                 prohibited
               </span>
@@ -131,85 +131,85 @@ function Rules() {
             </p>
           </div>
 
-          <div class="text6">
+          <div className="text6">
             <p>
               6. Speakers size above 6 inc is{" "}
-              <span class="txt6">prohibited,</span> loud system is not allowed
+              <span className="txt6">prohibited,</span> loud system is not allowed
               please respect the privacy{" "}
-              <span class="indent6">of other guests.</span>
+              <span className="indent6">of other guests.</span>
             </p>
           </div>
 
-          <div class="text7">
+          <div className="text7">
             <p>
               7. Resort security has the right to inspect every persoons bags,
               parcels or other items upon their{" "}
-              <span class="indent7">admission.</span>
+              <span className="indent7">admission.</span>
             </p>
           </div>
 
-          <div class="text8">
+          <div className="text8">
             <p>
               8. WE are a PET FRIENDLY resort and follows strict PET POLICY. (We
               allowed pets to enter only if{" "}
-              <span class="indent8">
+              <span className="indent8">
                 they were in their cage and wearing their pampers).
               </span>
             </p>
           </div>
 
-          <div class="text9">
+          <div className="text9">
             <p>
               9. Avoid littering. This is Clean and Green Resort. Common
               trashcans are available at the designed{" "}
-              <span class="indent9">places within the resort.</span>
+              <span className="indent9">places within the resort.</span>
             </p>
           </div>
 
-          <div class="text10">
+          <div className="text10">
             <p>
               10. The resort cannot be responsible for accident or injury of any
               of our guests, nor for the lost of{" "}
-              <span class="indent10">
+              <span className="indent10">
                 valuables, money or property of any kind.
               </span>
             </p>
           </div>
 
-          <div class="text11">
+          <div className="text11">
             <p>
               11. Prohibited drugs and firearms are not allowed. Please
               surrender Firearms to the front desk for{" "}
-              <span class="indent11">safekeeping.</span>
+              <span className="indent11">safekeeping.</span>
             </p>
           </div>
 
-          <div class="text12">
+          <div className="text12">
             <p>
-              12. Strictly <span class="txt12">NO TIME EXTENTION,</span> we
+              12. Strictly <span className="txt12">NO TIME EXTENTION,</span> we
               prioritize our cleaning breaks.
             </p>
           </div>
 
-          <div class="text13">
+          <div className="text13">
             <p>13. Prices are subject to change without prior notice.</p>
           </div>
 
-          <div class="text14">
+          <div className="text14">
             <p>
               14. We reserve the right to deny admission or evic guest or group
-              of guest who violate our <span class="indent14">policies.</span>
+              of guest who violate our <span className="indent14">policies.</span>
             </p>
           </div>
 
-          <div class="text15">
+          <div className="text15">
             <p>
               We are putting in a lot of effort to provide you the cleanest
               Resort and safety stay.
             </p>
           </div>
 
-          <div class="text16">
+          <div className="text16">
             <p>Thank you!</p>
           </div>
         </section>
@@ -218,9 +218,9 @@ function Rules() {
 
       {/* start of CTA */}
       <section id="cta">
-        <div class="cta-bg">
-          <div class="cta-content">
-            <div class="cta-above">
+        <div className="cta-bg">
+          <div className="cta-content">
+            <div className="cta-above">
               <h1>Experience the perfect getaway!</h1>
               <p>
                 Experience the perfect getaway at our resort, offering a range
@@ -228,9 +228,9 @@ function Rules() {
                 swimming rates for an unforgettable stay.
               </p>
             </div>
-            <div class="cta-below">
+            <div className="cta-below">
               <a href="reservation.html" id="cta-button">
-                <button type="button" class="cta-btn">
+                <button type="button" className="cta-btn">
                   BOOK NOW
                 </button>
               </a>
@@ -242,34 +242,34 @@ function Rules() {
 
       {/* start of footer */}
       <section id="footer">
-        <div class="ftr-container">
-          <div class="ftr-content">
-            <div class="ftr-left">
-              <img src={footerLogo} alt="logo" className="ftr-logo" />
-              <p class="cta-text">
+        <div className="ftr-container">
+          <div className="ftr-content">
+            <div className="ftr-left">
+              <img src={footerLogo} alt="logo" classNameName="ftr-logo" />
+              <p className="cta-text">
                 Book your unforgettable getaway now and experience the ultimate
                 relaxation at our resort!
               </p>
-              <div class="fb-link">
+              <div className="fb-link">
                 <a href="#" id="fblink">
                   <span>
                     <FontAwesomeIcon icon={faFacebook} id="ftricon" />
                   </span>
-                  <span class="label">Amaro Resort</span>
+                  <span className="label">Amaro Resort</span>
                 </a>
               </div>
-              <div class="email-address">
+              <div className="email-address">
                 <a href="#" id="emailadd">
                   <span>
                     <FontAwesomeIcon icon={faEnvelope} id="ftricon" />
                   </span>
-                  <span class="label">amaroresort@gmail.com</span>
+                  <span className="label">amaroresort@gmail.com</span>
                 </a>
               </div>
             </div>
 
-            <div class="ftr-right">
-              <div class="links-label">
+            <div className="ftr-right">
+              <div className="links-label">
                 <h2>Links</h2>
                 <ul>
                   <li>
@@ -289,7 +289,7 @@ function Rules() {
                   </li>
                 </ul>
               </div>
-              <div class="links-label">
+              <div className="links-label">
                 <h2>Legal</h2>
                 <ul>
                   <li>
@@ -303,14 +303,14 @@ function Rules() {
                   </li>
                 </ul>
               </div>
-              <div class="newsletter">
+              <div className="newsletter">
                 <h2>Newsletter</h2>
-                <p class="newsletter-text">
+                <p className="newsletter-text">
                   Elevate your resort experience and be the first to receive
                   exclusive promotions, exciting updates, and insider insights
                   by subscribing to our newsletter.
                 </p>
-                <div class="subscribe">
+                <div className="subscribe">
                 <input
                     type="email"
                     id="email"
@@ -324,7 +324,7 @@ function Rules() {
               </div>
             </div>
           </div>
-          <div class="copyright">Copyright © 2023. All rights reserved.</div>
+          <div className="copyright">Copyright © 2023. All rights reserved.</div>
         </div>
       </section>
       {/* end of footer */}
